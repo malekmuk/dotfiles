86d21ba72ccc36d72ce073bcc0cdac566e6e535d
malekmuk <mmukahhal896@gmail.com>
2024-02-07 22:34:54 -0600

    init

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+    { "martinsione/darkplus.nvim" },
+
+    { "projekt0n/github-nvim-theme" },
+
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "github_dark_default",
+        },
+    },
+}
